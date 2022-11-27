68c3f5975050637872e8958161322c7c9b1738e4
Nakul kankarwal <79783298+nakulk02@users.noreply.github.com>
2022-11-27 15:04:14 +0530

    Update Q1.java

---
+++ assignment6/Q1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Question_1 {
+	
+	public static double division(int num1, int num2) { 
+		if(num1 == 0 || num2 == 0) { 
+			throw new ArithmeticException("Exception: numbers should not be zero");
+		}
+		else {
+		    double ans = (double) num1 / num2;  
+		    System.out.println(ans);
+		    return ans;
+		}
+	}
+	
+	public static int multiply(int num1, int num2) {
+		
+		if(num1 == 0 || num2 == 0) { 
+			throw new ArithmeticException("Exception: numbers should not be zero");
+		}
+		else {
+			int ans = num1 * num2; 
+		    System.out.println(ans); 
+		    return ans;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		
+		int num1 = scn.nextInt();    
+		int num2 = scn.nextInt();
+		
+		try { 
+			division(num1, num2);   			
+			multiply(num1, num2);
+		}
+		catch(ArithmeticException e) { 
+			System.out.println(e);
+		}
+	}
+
+}
